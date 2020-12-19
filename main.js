ddfe3b1c3f88246bada9fa9d53c94bdcb4063a23
medzvre <61162234+medzvre@users.noreply.github.com>
2020-12-20 01:04:13 +0400

    first deploy

---
+++ main.js
@@ -0,0 +1,109 @@
+let content = document.getElementById('content');
+let score = 0;
+let quize = 0;
+let isClickedOnSubmit = 0;
+
+
+
+window.onload = function() {
+    document.getElementById('submit').style.opacity = 1
+    content.innerHTML = getHtml()
+    randomizeAnswerOrder()
+}
+
+function randomizeAnswerOrder() {
+    let answers = document.querySelectorAll('.answers');
+    let randomIndex = Math.round(Math.random() * 3)
+    let tmpPosition = answers[randomIndex].innerHTML
+    answers[randomIndex].innerHTML = ''
+    answers[randomIndex].innerHTML = answers[0].innerHTML
+    answers[0].innerHTML = tmpPosition
+}
+
+function getHtml() {
+    return `
+    <div class="top">
+    <h3 class="question">${questions[quize].question} ?</h3>
+    <img src="${questions[quize].img}" alt="">
+</div>
+<div class="bottom">
+    <div class="answers a"><p onclick='selectClickedElement(this)' class="answer">${questions[quize].correctAnswer}</p></div>
+    <div class="answers b"><p onclick='selectClickedElement(this)' class="answer">${questions[quize].wrongAnswer1}</p></div>
+    <div class="answers c"><p onclick='selectClickedElement(this)' class="answer">${questions[quize].wrongAnswer2}</p></div>
+    <div class="answers d"><p onclick='selectClickedElement(this)' class="answer">${questions[quize].wrongAnswer3}</p></div>
+</div>
+`
+}
+
+function selectClickedElement(element) {
+    let answers = document.querySelectorAll('.answers');
+    answers.forEach(elements => {
+            elements.style.background = 'rgba(0, 0, 0, 0.5)';
+            elements.style.color = 'white';
+        }) //when click on element turn it yellow
+    if (isClickedOnSubmit == 0) {
+        element.parentElement.style.background = 'rgb(249, 221, 7)'; //Yellow
+        element.parentElement.style.color = 'rgba(0, 0, 0, 0.5)';
+    } else {
+        alert('you can not change your answer after submiting')
+        document.getElementById('submit').textContent = 'Submit Answer'
+
+        quize++
+        content.innerHTML = ''
+        if (quize < questions.length) {
+            content.innerHTML = getHtml()
+        } else {
+            content.innerHTML = `<div class="score">Your Score is: ${score}</div>`
+        }
+        randomizeAnswerOrder()
+        isClickedOnSubmit = 0
+    }
+}
+
+
+
+function submitAnswer(thisElement) {
+    let answers = document.querySelectorAll('.answers');
+
+    if (isClickedOnSubmit == 0) {
+        answers.forEach(elements => {
+            if (elements.style.background == 'rgb(249, 221, 7)') { //Yellow
+                if (elements.querySelector('p').textContent == questions[quize].correctAnswer) {
+                    elements.style.background = 'rgb(19, 221, 19)'; //Green
+                    score++
+                    console.log(score)
+                } else {
+                    elements.style.background = 'rgb(221, 19, 19)'; //red
+                }
+            }
+        })
+    }
+    goToTheNextQuestion(thisElement)
+}
+
+function goToTheNextQuestion(thisElement) {
+    let answers = document.querySelectorAll('.answers');
+    isClickedOnSubmit++
+    thisElement.textContent = 'Next'
+    if (isClickedOnSubmit == 2) {
+        answers.forEach(elements => {
+            elements.style.background = 'rgba(0, 0, 0, 0.5)';
+            elements.style.color = 'white';
+            thisElement.textContent = 'Submit Answer'
+        })
+        quize++
+        content.innerHTML = ''
+        if (quize < questions.length) {
+            content.innerHTML = getHtml()
+        } else {
+            content.innerHTML = `<section class='last-section'>
+            <div class="score">Your Score is: ${score}</div>
+            <a class='goToHomePage' href="index.html">Go to the Home Page</a>
+            </section>
+            `
+            document.getElementById('submit').style.opacity = 0
+        }
+        randomizeAnswerOrder()
+        isClickedOnSubmit = 0
+    }
+}
---
+++ medicineQuizeQuestions.js
@@ -0,0 +1,81 @@
+let questions = [{
+        question: 'რამდენჯერ ცემს გული დღის განმავლობაში',
+        img: './medicineQuizeAssets/binaries.png',
+        correctAnswer: '86 000 - 144 000',
+        wrongAnswer1: '60 000 - 77 000',
+        wrongAnswer2: '155 000 - 200 000',
+        wrongAnswer3: '15 000 - 35 000 '
+    },
+    {
+        question: 'რომელია სწორი დებულება',
+        img: './medicineQuizeAssets/question.jpg',
+        correctAnswer: ' ადამიანს საკვების მიღების შემდეგ შედარებით ცუდად ესმის',
+        wrongAnswer1: 'ადამიანი დაცემინებისას წონაში ილებს 200გრ-ით ',
+        wrongAnswer2: 'სუნის არქმის უნარი დაბადებიდან კაცებს უფრო განვითარებული აქვთ ვიდრე ქალებს',
+        wrongAnswer3: 'დაცემინების სისწრაფე შეადგენს 200კმ საათს'
+    },
+    {
+        question: 'რამდენი ძვლისგან შედგება ადამიანის ჩონჩხი დაბადებისას',
+        img: './medicineQuizeAssets/babyBones.jpg',
+        correctAnswer: '270',
+        wrongAnswer1: '190',
+        wrongAnswer2: '170',
+        wrongAnswer3: '208'
+    },
+    {
+        question: 'ადამიანის სხვეულის რომელ ნაწილში მდებარეობს ყველაზე მცირე ზომის ძვლები',
+        img: './medicineQuizeAssets/bones.jpg',
+        correctAnswer: 'ყურებში',
+        wrongAnswer1: 'თითებში',
+        wrongAnswer2: 'ხელებში',
+        wrongAnswer3: 'ფეხებში'
+    },
+    {
+        question: 'ადამიანის ორგანიზმის რამდენ პროცენტს შეადგენს წყალი',
+        img: './medicineQuizeAssets/water.jpg',
+        correctAnswer: '60%',
+        wrongAnswer1: '70%',
+        wrongAnswer2: '69%',
+        wrongAnswer3: '74%'
+    },
+    {
+        question: 'თითის ანაბეჭდის გარდა კიდევ რომელ ორგანოს აქვს უნიკალური ანაბეჭდი',
+        img: './medicineQuizeAssets/faceRecognition.jpg',
+        correctAnswer: 'ენას',
+        wrongAnswer1: 'ცხვირს',
+        wrongAnswer2: 'ლოყას',
+        wrongAnswer3: 'ყურს'
+    },
+    {
+        question: 'რამდენ ხანს გაგრძელდა ისტორიაში ყველაზე ხანგრძლივი ორსულობა',
+        img: './medicineQuizeAssets/pregnancy.jpg',
+        correctAnswer: '12,5 თვე',
+        wrongAnswer1: '10,5 თვე',
+        wrongAnswer2: '11 თვე',
+        wrongAnswer3: '10 თვე'
+    },
+    {
+        question: 'რამდენი კუნთი მუშაობს ერთი ნაბიჯის გადადგმის დროს  ',
+        img: './medicineQuizeAssets/step.jpg ',
+        correctAnswer: '200',
+        wrongAnswer1: '150',
+        wrongAnswer2: '180',
+        wrongAnswer3: '201'
+    },
+    {
+        question: 'თავდაპირველად რის სამსკურნალოდ გამოიყენებოდა კოკაინი',
+        img: './medicineQuizeAssets/cocain.jpg',
+        correctAnswer: ' ხველების',
+        wrongAnswer1: 'დეპრესიის',
+        wrongAnswer2: 'ფილტვების',
+        wrongAnswer3: 'უენერგიობის'
+    },
+    {
+        question: 'წარმოშობით საიდან იყო ჰიპოკრატე',
+        img: './medicineQuizeAssets/hipokrate.jpg',
+        correctAnswer: 'საბერძნეთი',
+        wrongAnswer1: 'რომი',
+        wrongAnswer2: 'მაკედონია',
+        wrongAnswer3: 'ეგვიფტე'
+    }
+]
